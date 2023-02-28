b3eddc92279cf45689912c1b1ab75fc760263b25
wachira141 <wachiraw75@gmail.com>
2023-02-28 05:40:15 -0800

    initial commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""
-query Reddit API and prints the titles of the 
-first 10 hot posts listed
-"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    titles of the first 10
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-    r = requests.get(url, headers=headers).json()
-    tp_ten = r.get('data', {}).get('children', [])
-    if not tp_ten:
-        print(None)
-    for t in tp_ten:
-        print(t.get('data').get('title'))
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
