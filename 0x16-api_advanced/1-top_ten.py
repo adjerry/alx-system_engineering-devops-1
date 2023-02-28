5a52d6bd043402d4f0c75eb94a7bd15c13a7068b
wachira141 <wachiraw75@gmail.com>
2023-02-28 04:20:47 -0800

    Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+query Reddit API and prints the titles of the 
+first 10 hot posts listed
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    titles of the first 10
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+    r = requests.get(url, headers=headers).json()
+    tp_ten = r.get('data', {}).get('children', [])
+    if not tp_ten:
+        print(None)
+    for t in tp_ten:
+        print(t.get('data').get('title'))
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Querrying REDDIT API
