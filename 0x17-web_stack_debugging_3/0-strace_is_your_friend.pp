25970c546175f9924f01528610c3ac766452579f
wachira141 <wachiraw75@gmail.com>
2023-03-02 01:43:17 -0800

    initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Fix a 500 internal server error
+
+exec { 'fix-wordpress':
+    command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
+    path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+Debugging a buggy Wordpress website that fails with 500 internal server Error.
+Automate using puppet
